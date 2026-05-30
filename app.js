6997039bf4c2ed7f29b8f8eba61e3e395e3a0af9
Naveen.Patibandla <Naveen.Patibandla@vistex.com>
2026-05-30 12:52:53 +0530

    basic project setup

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.status(200).json({message:"Hellow from server side",app:"Natours"});
+});
+app.post('/',(req,res)=>{
+    res.send("You can post to this endpoint...")
+})
+app.listen(port,()=>{
+    console.log(`App is running on port ${port}...`);
+})
